bc7cd74145c9a19e352c106a058621fe43be56a2
Tryd0g0lik <work80@mail.ru>
2024-10-24 13:57:59 +0700

    [master]:Tree updated

--- src/sripts/listener.ts
+++ src/sripts/listener.ts
@@ -1,11 +1,10 @@
 
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  /* сделать прослушку для к "#submit-repeat". Через "fetch"
-  запросить "/repeat_token" и все. Даьше заработает код python из
-  "referral/views_more/views_account.py"
-  */
-
+  //   /* сделать прослушку для к "#submit-repeat". Через "fetch"
+  //   запросить "/repeat_token" и все. Даьше заработает код python из
+  //   "referral/views_more/views_account.py"
+  //   */
+  sessionStorage.clear()
 })
